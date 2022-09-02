dceb265506eaf0619f71b86d7c4c402b2ae599d5
JAHID AL MAMUN <jahid@oneixchange.com>
2022-09-02 10:00:51 +0600

    And Design add and check

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h3>Ant design implement and test</h3>
+        <DatePicker />
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
